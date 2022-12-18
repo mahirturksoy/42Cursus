c3ff381588e66fc7178dd33c5fae453d56d09f48
Mahir Türksoy <108736027+mahirturksoy@users.noreply.github.com>
2022-12-18 11:34:40 +0300

    Update ft_itoa.c

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -1,27 +1,43 @@
 #include "libft.h"
 
-char *ft_itoa(int n)
+static size_t	len_digits(int n)
 {
-    int len;
-    char *ret;
-    const char *digits = "0123456789";
+	size_t	count;
 
-    len = ft_numlen(n, 10);
-    ret = malloc(sizeof(char) * (len + 1));
-    if(!ret)
-        return(0);
-    ret[len] = 0;
-    if(n == 0)
-        ret[0] = '0';
-    if(n < 0)
-        ret[0] = '-';
-    while(n)
-    {
-        if(n > 0)
-            ret[--len] = digits[n % 10];
-        else
-            ret[--len] = digits[n % 10 * -1];
-        n /= 10;
-    }
-    return(ret);
+	count = 0;
+	if (n < 0 || n == 0)
+		count++;
+	while (n)
+	{
+		n /= 10;
+		count++;
+	}
+	return (count);
+}
+
+char	*ft_itoa(int n)
+{
+	char		*str;
+	size_t		slen;
+	const char	*digits;
+
+	digits = "0123456789";
+	slen = len_digits(n);
+	str = (char *)malloc(sizeof(char) * (slen + 1));
+	if (!str)
+		return (NULL);
+	str[slen] = '\0';
+	if (n == 0)
+		str[0] = '0';
+	if (n < 0)
+		str[0] = '-';
+	while (n)
+	{
+		if (n > 0)
+			str[--slen] = digits[n % 10];
+		else
+			str[--slen] = digits[n % 10 * (-1)];
+		n /= 10;
+	}
+	return (str);
 }
