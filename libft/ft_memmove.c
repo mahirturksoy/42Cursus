0ff156d11920a4fcd1a035301e736f96fa6c537e
Mahir Türksoy <108736027+mahirturksoy@users.noreply.github.com>
2022-12-18 14:01:57 +0300

    Update ft_memmove.c

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -1,27 +1,23 @@
 #include "libft.h"
 
-void *ft_memmove(void *dst, const void *src, size_t len)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-    size_t index;
+	unsigned char	*d;
+	unsigned char	*s;
 
-    if(dst == src || !len)
-        return (dst);
-    index = 0;
-    if(dst < src)
-    {
-        while(index < len)
-        {
-            *((char*)dst + index) = *((char*)src + index);
-            index++;
-        }
-    }
-    else
-    {
-        while(len > 0)
-        {
-            *((char *)dst + len - 1) = *((char*)src + len - 1);
-            len--;
-        }
-    }
-    return (dst);
+	if (!n || src == dest)
+		return (dest);
+	d = (unsigned char *)dest;
+	s = (unsigned char *)src;
+	if (dest < src)
+	{
+		while (n--)
+			*d++ = *s++;
+	}
+	else
+	{
+		while (n--)
+			d[n] = s[n];
+	}
+	return (dest);
 }
