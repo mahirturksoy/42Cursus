4ba11845b28a0a45f79aab2a4ee9352682f0b83e
Mahir Türksoy <108736027+mahirturksoy@users.noreply.github.com>
2022-12-22 13:03:04 +0300

    Update ft_strmapi.c

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -1,24 +1,21 @@
 #include "libft.h"
 
- char *ft_strmapi(char const *s, char(*f)(unsigned int, char))
- {
-    char *str;
-    size_t s_strlen;
-    size_t i;
+ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char			*str;
 
-    if(!s)
-        return(0);
-    i = 0;
-    s_strlen = ft_strlen(s);
-    str = malloc(sizeof(char) * (s_strlen + 1));
-    if(!str)
-        return(NULL);
-    while(*s)
-    {
-        str[i] = f(i,(char)*s);
-        s++;
-        i++;
-    }
-    str[i] = '\0';
-    return (str);
- }
+	if (!s)
+		return (NULL);
+	i = 0;
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (str == NULL)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
